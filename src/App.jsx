8a6a783b44f9c548e5c6e05edffcb8b822ba263c
TahaSyedKhan <tahasyedkhan123@gmail.com>
2025-06-25 14:56:18 +0530

    created Avatar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Avatar from "./Components/Avatar";
 import Button from "./Components/Button";
 
 export default function App() {
@@ -7,6 +8,11 @@ export default function App() {
       <Button size='sm' variant='success'>Log in with Google</Button>
       <Button size='lg' variant='warning'>Log in with Google</Button>
       <Button size='lg' variant='danger'>Log in with Google</Button>
+      <section className="avatar-section">
+        <Avatar src="../images/bob.jpg" alt="avatar-image"/>
+        <Avatar>BZ</Avatar>
+        <Avatar />
+      </section>
     </main>
   )
 }
---
+++ src/Components/Avatar.jsx
@@ -0,0 +1,35 @@
+import { IoPersonSharp } from "react-icons/io5"
+
+const colorClasses = ['navy', 'pink', 'red', 'blue', 'green']
+const randomColor = Math.floor(Math.random() * colorClasses.length)
+const randomColorClass = colorClasses[randomColor]
+
+export default function Avatar({src, alt, children}) {
+    function renderAvatarDivs() {
+        if(src) {
+            return (
+                <div className="avatar">
+                    <img src={src} alt={alt} />
+                </div>
+            )
+        }
+        if(children) {
+            return (
+                <div className={`avatar avatar-letters ${randomColorClass}`}>
+                    {children}
+                </div>
+            )
+        } else {
+            return (
+                <div className={`avatar avatar-icons ${randomColorClass}`}>
+                    <IoPersonSharp />
+                </div>
+            )
+        }
+    }
+    return (
+        <>
+            {renderAvatarDivs()}
+        </>
+    )
+}
